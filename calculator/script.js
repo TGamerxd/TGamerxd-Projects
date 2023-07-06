bc5870b710fdec869ea3e805ae5e8954376625f2
TGamerxd <90946491+TGamerxd@users.noreply.github.com>
2023-07-06 02:31:35 -0700

    Add files via upload

--- calculator/script.js
+++ calculator/script.js
@@ -18,3 +18,22 @@ function calculate(){
     var y = eval(x);
     document.getElementById("result").value = y;
 }
+
+const theme = document.getElementById("theme");
+const elements = document.querySelectorAll('.heads, .logo, .calculator,.display,.cls,.inputbox,.row,.act,.heading,.thm');
+var sun = document.getElementById("icon");
+
+theme.addEventListener('click',function(){
+    console.log("test dark theme")
+    document.body.classList.toggle('dark');
+    elements.forEach(function(element){
+        element.classList.toggle('dark');
+    })
+    if(icon.classList.contains("dark")){
+        icon.classList.remove("fa-sun");
+        icon.classList.add("fa-moon");
+    }else{
+        icon.classList.remove("fa-moon");
+        icon.classList.add("fa-sun");
+    }
+})
