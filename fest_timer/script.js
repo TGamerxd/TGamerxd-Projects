51aab811f7d158851265ea5d0813da7aa318c441
TGamerxd <90946491+TGamerxd@users.noreply.github.com>
2023-11-23 18:45:39 +0530

    Update script.js

--- fest_timer/script.js
+++ fest_timer/script.js
@@ -1,14 +1,21 @@
 function updateTimer() {
     const now = new Date();
-    const targetTime = new Date(now.getFullYear(), now.getMonth(), now.getDate(), 19, 30, 0); // 7:30 PM
-    
-    const timeDiff = targetTime - now;
 
-    const hours = Math.floor(timeDiff / (1000 * 60 * 60));
-    const minutes = Math.floor((timeDiff % (1000 * 60 * 60)) / (1000 * 60));
-    const seconds = Math.floor((timeDiff % (1000 * 60)) / 1000);
+    // Specify the target time in GMT
+    const targetTimeGMT = new Date(now);
+    targetTimeGMT.setUTCHours(14, 0, 0); // 2:00 PM GMT
 
-    document.getElementById('timer').innerHTML = `${hours}h ${minutes}m ${seconds}s`;
+    const timeDiff = targetTimeGMT - now;
+
+    if (timeDiff <= 0) {
+        document.getElementById('timer').innerHTML = "Event has started!";
+    } else {
+        const hours = Math.floor(timeDiff / (1000 * 60 * 60));
+        const minutes = Math.floor((timeDiff % (1000 * 60 * 60)) / (1000 * 60));
+        const seconds = Math.floor((timeDiff % (1000 * 60)) / 1000);
+
+        document.getElementById('timer').innerHTML = `${hours}h ${minutes}m ${seconds}s`;
+    }
 }
 
 // Update the timer every second
