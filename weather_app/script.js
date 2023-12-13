fca6c237fd561291376daff6035fa07f0f684b47
TGamerxd <90946491+TGamerxd@users.noreply.github.com>
2023-12-13 10:06:37 +0530

    Add files via upload

--- weather_app/script.js
+++ weather_app/script.js
@@ -28,7 +28,92 @@ const getWeather = (city) => {
             sunset.innerHTML = formatUnixTimestamp(response.sunset)
         })
         .catch(err => console.error(err));
+    //new york weather data down    
+    fetch('https://weather-by-api-ninjas.p.rapidapi.com/v1/weather?city=' + "New York", options)
+        .then(response => response.json())
+        .then((response) => {
+
+            console.log(response)
+
+            // cloud_pct.innerHTML = response.cloud_pct
+            tempn.innerHTML = response.temp
+            feels_liken.innerHTML = response.feels_like
+            humidityn.innerHTML = response.humidity
+            // humidity2.innerHTML = response.humidity
+            min_tempn.innerHTML = response.min_temp
+            max_tempn.innerHTML = response.max_temp
+            wind_speedn.innerHTML = response.wind_speed
+            // wind_speed2.innerHTML = response.wind_speed
+            // wind_degreesn.innerHTML = response.wind_degrees
+            sunrisen.innerHTML = formatUnixTimestamp(response.sunrise)
+            sunsetn.innerHTML = formatUnixTimestamp(response.sunset)
+        })
+        .catch(err => console.error(err));
+    //delhi weather down
+    fetch('https://weather-by-api-ninjas.p.rapidapi.com/v1/weather?city=' + "Delhi", options)
+        .then(response => response.json())
+        .then((response) => {
+
+            console.log(response)
+
+            // cloud_pct.innerHTML = response.cloud_pct
+            tempd.innerHTML = response.temp
+            feels_liked.innerHTML = response.feels_like
+            humidityd.innerHTML = response.humidity
+            // humidity2.innerHTML = response.humidity
+            min_tempd.innerHTML = response.min_temp
+            max_tempd.innerHTML = response.max_temp
+            wind_speedd.innerHTML = response.wind_speed
+            // wind_speed2.innerHTML = response.wind_speed
+            // wind_degreesn.innerHTML = response.wind_degrees
+            sunrised.innerHTML = formatUnixTimestamp(response.sunrise)
+            sunsetd.innerHTML = formatUnixTimestamp(response.sunset)
+        })
+        .catch(err => console.error(err));
+    //tokyo weather down
+    fetch('https://weather-by-api-ninjas.p.rapidapi.com/v1/weather?city=' + "tokyo", options)
+        .then(response => response.json())
+        .then((response) => {
+
+            console.log(response)
+
+            // cloud_pct.innerHTML = response.cloud_pct
+            tempt.innerHTML = response.temp
+            feels_liket.innerHTML = response.feels_like
+            humidityt.innerHTML = response.humidity
+            // humidity2.innerHTML = response.humidity
+            min_tempt.innerHTML = response.min_temp
+            max_tempt.innerHTML = response.max_temp
+            wind_speedt.innerHTML = response.wind_speed
+            // wind_speed2.innerHTML = response.wind_speed
+            // wind_degreesn.innerHTML = response.wind_degrees
+            sunriset.innerHTML = formatUnixTimestamp(response.sunrise)
+            sunsett.innerHTML = formatUnixTimestamp(response.sunset)
+        })
+        .catch(err => console.error(err));
+    //london weather down
+    fetch('https://weather-by-api-ninjas.p.rapidapi.com/v1/weather?city=' + "tokyo", options)
+        .then(response => response.json())
+        .then((response) => {
+
+            console.log(response)
+
+            // cloud_pct.innerHTML = response.cloud_pct
+            templ.innerHTML = response.temp
+            feels_likel.innerHTML = response.feels_like
+            humidityl.innerHTML = response.humidity
+            // humidity2.innerHTML = response.humidity
+            min_templ.innerHTML = response.min_temp
+            max_templ.innerHTML = response.max_temp
+            wind_speedl.innerHTML = response.wind_speed
+            // wind_speed2.innerHTML = response.wind_speed
+            // wind_degreesn.innerHTML = response.wind_degrees
+            sunrisel.innerHTML = formatUnixTimestamp(response.sunrise)
+            sunsetl.innerHTML = formatUnixTimestamp(response.sunset)
+        })
+        .catch(err => console.error(err));
 }
+
 
 const formatUnixTimestamp = (timestamp) => {
     const date = new Date(timestamp * 1000);
