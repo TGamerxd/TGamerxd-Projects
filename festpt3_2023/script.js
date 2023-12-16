e1b6e9023d9ff059f4a2147b24317e5b75ef076e
TGamerxd <90946491+TGamerxd@users.noreply.github.com>
2023-12-16 13:38:48 +0530

    Add files via upload

---
+++ festpt3_2023/script.js
@@ -0,0 +1,21 @@
+const targetDate = new Date('December 26, 2023 14:00:00 GMT');
+
+  function updateCountdown() {
+    const now = new Date().getTime();
+    const timeRemaining = targetDate - now;
+
+    if (timeRemaining > 0) {
+      const days = Math.floor(timeRemaining / (1000 * 60 * 60 * 24));
+      const hours = Math.floor((timeRemaining % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+      const minutes = Math.floor((timeRemaining % (1000 * 60 * 60)) / (1000 * 60));
+      const seconds = Math.floor((timeRemaining % (1000 * 60)) / 1000);
+
+      document.getElementById('countdown').innerHTML = `${days}D ${hours}H ${minutes}M ${seconds}S`;
+    } else {
+      document.getElementById('countdown').innerHTML = 'Time\'s up!';
+    }
+  }
+
+  setInterval(updateCountdown, 1000);
+
+  updateCountdown();
